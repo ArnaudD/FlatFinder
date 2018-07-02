11f3cbc597a99161c66179f7e24239d32ef77e3c
Arnaud Didry <arnaud@fizix.io>
2018-07-02 22:41:46 +0200

    Update LBC scrapper + use browerless/chromeless

--- scrape.js
+++ scrape.js
@@ -2,25 +2,42 @@
 import puppeteer from 'puppeteer';
 
 export default async function (url, options) {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
+
+  const browser = await puppeteer.connect({ browserWSEndpoint: 'ws://chromeless:3000' });
+  let results = [];
 
   try {
-    await page.goto(url, { timeout: 0 });
-  } catch (e) {}
+    console.log(`loading url "${url}"`);
+    const page = await browser.newPage();
 
-  await page.waitForSelector(options.waitForSelector);
+    try {
+      await page.goto(url, { timeout: 0 });
+    } catch (e) {
+      // no timeout, we will wait in `waitForSelector`
+    }
 
-  await page.injectFile('node_modules/jquery/dist/jquery.min.js');
-  await page.injectFile('node_modules/artoo-js/build/artoo.chrome.js');
+    console.log(`waiting for selector`);
+    await page.waitForSelector(options.waitForSelector);
 
-  const results = await page.evaluate(({ parent, items }) => {
-    artoo.jquery.force = false;
-    artoo.$ = window.jQuery;
-    return artoo.scrape(parent, items);
-  }, options);
+    console.log(`injecting artoo + jquery`);
+    await page.injectFile('node_modules/jquery/dist/jquery.min.js');
+    await page.injectFile('node_modules/artoo-js/build/artoo.chrome.js');
 
-  await browser.close();
+    console.log(`running artoo`);
+
+    if (options.evaluate) {
+      results = await page.evaluate(options.evaluate);
+    } else {
+      results = await page.evaluate(({ parent, items }) => {
+        artoo.jquery.force = false;
+        artoo.$ = window.jQuery;
+        return artoo.scrape(parent, items);
+      }, options);
+    }
+
+  } finally {
+    await browser.close();
+  }
 
   return results.map(options.mapResults);
 };
--- sources/leboncoin.js
+++ sources/leboncoin.js
@@ -1,21 +1,16 @@
 import _ from 'lodash';
 
 export default {
-  waitForSelector: '.tabsContent',
-  parent: '.tabsContent li',
-  items: {
-    title: {sel: 'h2.item_title'},
-    url: {sel: 'a', attr: 'href'},
-    image: {sel: '.item_imagePic > span', attr: 'data-imgSrc' },
-    price: {sel: '.item_price', attr:  'content'},
-    date: {sel: '.item_supp[itemprop="availabilityStarts"]', attr:  'content'},
-    time: {sel: '.item_supp[itemprop="availabilityStarts"]'},
+  waitForSelector: '.react-tabs__tab-panel',
+  evaluate: () => {
+    return window.FLUX_STATE.adSearch.data.ads;
   },
   mapResults: result => ({
     ...result,
-    url: `https:${result.url}`,
-    title: _.trim(result.title),
-    id: result.url.match(/locations\/(.*)\./)[1],
-    time: result.time.match(/([0-9]{2}:[0-9]{2})/)[1],
+    title: _.trim(result.subject),
+    price: _.get(result, ['price', 0]),
+    id: result.list_id,
+    time: result.first_publication_date,
+    image: _.get(result, ['images', 'urls_large', 0]),
   }),
 };
